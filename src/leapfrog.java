bbd919027ed3f129e88e111df9995be16c9476da
Boonk3312 <theboonk7@hotmail.com>
2024-03-16 22:25:33 -0700

    Create leapfrog.java

---
+++ src/leapfrog.java
@@ -0,0 +1,15 @@
+public class Leapfrog {
+    private String cmake;
+    private String leapfrogOfficial;
+    private static String leapfrog;
+
+    public Leapfrog(int value) {
+        this.cmake = "cmake";
+        this.leapfrogOfficial = "unofficial";
+    }
+
+    public static void main(String[] args) {
+        leapfrog = "f4910bbafde9cffa5e72bbf6e8d0a9bbfd8eaee1329480038bbfd6";
+        Leapfrog leapObject = new Leapfrog(1);
+    }
+}
